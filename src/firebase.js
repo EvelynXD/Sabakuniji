572673198ef3589912ab1c0f1eb848c381af21d3
EvelynXD <91973757+EvelynXD@users.noreply.github.com>
2023-10-12 15:44:51 +0700

    firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -10,12 +10,12 @@ import {getAuth, GoogleAuthProvider} from 'firebase/auth'
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBmUIXJblRGF4HTCO3hOB8MykvLOq7whUE",
-  authDomain: "project-web-kelas.firebaseapp.com",
-  projectId: "project-web-kelas",
-  storageBucket: "project-web-kelas.appspot.com",
-  messagingSenderId: "202691312352",
-  appId: "1:202691312352:web:e82e7a494e705b2a12ee85"
+  apiKey: "AIzaSyDGd_8Z7MUM2XCHud03uJTWcP2ZneQEFMs",
+  authDomain: "projek-kelas-1c.firebaseapp.com",
+  projectId: "projek-kelas-1c",
+  storageBucket: "projek-kelas-1c.appspot.com",
+  messagingSenderId: "177371632187",
+  appId: "1:177371632187:web:1a115bad68da393828821e"
 };
 
 // Initialize Firebase
